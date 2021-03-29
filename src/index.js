1bf2e4fb3794626931b358ec5016987506781eb3
KellyGc99 <gutierrezkelly88@gmail.com>
2021-03-29 10:24:06 -0500

    agregar cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import validator from './validator.js';
 
-console.log(validator);
+/* Para mostrar solo la ventana de inicio */
+
+document.getElementById('fondoTienda').style.display='none';
+document.getElementById('fondoTarjeta').style.display='none';
+
+/* Para mostrar la vista de la tienda y ocultar la vista de inicio y la de tarjeta de crédito*/
+
+let comprar=document.getElementById('botonTienda');
+comprar.addEventListener('click',comprarTienda);
+
+function comprarTienda() {
+document.getElementById('fondoTienda').style.display='block';
+document.getElementById('fondoTarjeta').style.display='none';
+document.getElementById('fondoPaginaUno').style.display='none';
+}
+
+/*Se selecciona el articulo */
+
+let tarjetaPagarDragopavo=document.getElementById('pagarDragopavo');
+tarjetaPagarDragopavo.addEventListener('click',ingresarTarjeta);
+let tarjetaPagarMulagua=document.getElementById('pagarMulagua');
+tarjetaPagarMulagua.addEventListener('click',ingresarTarjeta);
+let tarjetaPagarActitud=document.getElementById('pagarActitud');
+tarjetaPagarActitud.addEventListener('click',ingresarTarjeta);
+
+/*Acá se muestra la vista de la tarjeta de crédito */
+
+function ingresarTarjeta(){
+document.getElementById('fondoTienda').style.display='none';
+document.getElementById('fondoTarjeta').style.display='block';
+document.getElementById('fondoPaginaUno').style.display='none';
+}
+
+/*Permite regresar a la vista de la tienda si se quiere cambiar de articulo*/
+
+let regresar=document.getElementById('botonParaRegresarATienda');
+regresar.addEventListener('click',volverTienda);
+
+function volverTienda(){
+document.getElementById('fondoTienda').style.display='block';
+document.getElementById('fondoTarjeta').style.display='none';
+document.getElementById('fondoPaginaUno').style.display='none';
+}
+
+/*mensaje sobre si el numero de tarjeta es correcto o no */
+
+/*let valorDeLaTarjeta= document.getElementById('enviarCodigoDeTarjeta').value;
+valorDeLaTarjeta.addEventListener('click',mensajeSobreLaTarjeta);
+
+function mensajeSobreLaTarjeta(){
+    if(){
+        alert=("El número (maskify) es válido . Compra realizada con éxito");
+    }else if(){
+        alert=("No se puede verificar, faltan dígitos en tu tarjeta");
+    }else(){
+        alert=("El número de la tarjeta es invalido, pruebe ingresando otro");
+    }
+}*/
+
+/*Maskify*/
+const numeroTarjeta=document.getElementById("numeroDeTarjeta");
+const llamarBoton=document.getElementById("enviarCodigoDeTarjeta");
+llamarBoton.addEventListener('click',procesar);
+
+
+function procesar(){
+    //const valorEnmascarado=validator.maskifySinFor(numeroTarjeta.value);
+    const valorEnmascarado=validator.maskify(numeroTarjeta.value);
+    alert(`El numero ${valorEnmascarado} es válido.`);
+}
+/*isValid*/
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,38 @@
 const validator = {
-  // ...
-};
+    maskifySinFor: function (creditCardNumber){
+      let remplazar= Array.from(creditCardNumber);
+      let mascara =remplazar.fill("#", 0, remplazar.length-4);
+      const resultado=mascara.join(" ");
+      return resultado;
+      
+      /*isValid(remplazar);*/
+    },
+    maskify: function (creditCardNumber){
+      let resultado=""; 
+      let tamaño=creditCardNumber.length-4;
+      for (let dato = 0; dato < tamaño; dato++) {
+      resultado=resultado+"#";
+      }
+      for (let i= tamaño; i<creditCardNumber.length; i++){
+        resultado=resultado+creditCardNumber[i];
+      }
+      return resultado;
+    }
+}
+//isValid 
+/*let str = '';
 
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+console.log(str);*/
 export default validator;
+
+
+
+/*validator.isValid(creditCardNumber): creditCardNumber es un string con el número de tarjeta que se va a verificar. Esta función debe retornar un boolean dependiendo si es válida de acuerdo al algoritmo de Luhn.
+
+validator.maskify(creditCardNumber): creditCardNumber es un string con el número de tarjeta y esta función debe retornar un string donde todos menos los últimos cuatro caracteres sean reemplazados por un numeral (#) o 🐱. 
+Esta función deberá siempre mantener los últimos cuatro caracteres intactos, aún cuando el string sea de menor longitud.*/
+
